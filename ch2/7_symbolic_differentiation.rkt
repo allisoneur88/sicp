e1e0946e4a7b369ab4469657d02b3b18ac4cfedf
allisoneur88 <adganshin@gmail.com>
2025-12-15 00:31:24 +0100

    symb diff with reduction of sum and product, also ex. 2.56

--- ch2/7_symbolic_differentiation.rkt
+++ ch2/7_symbolic_differentiation.rkt
@@ -18,17 +18,38 @@
                                        (deriv (multiplicand exp) var))
                          (make-product (deriv (multiplier exp) var)
                                        (multiplicand exp))))
+        ((exponentiation? exp) (make-product
+                                (make-product (exponent exp)
+                                              (make-exponentiation (base exp) (- (exponent exp) 1)))
+                                (deriv (base exp) var)))
         (else
          (error "unknown expression type: DERIV" exp))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
 
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
 
-(define (make-product m1 m2) (list '* m1 m2))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponentiation base exp)
+  (cond ((=number? exp 0) 1)
+        ((=number? exp 1) base)
+        (else (list '^ base exp))))
 
 (define (sum? x) (and (pair? x) (eq? (car x) '+)))
 
@@ -42,4 +63,13 @@
 
 (define (multiplicand p) (caddr p))
 
+(define (exponentiation? x) (and (pair? x) (eq? (car x) '^)))
+
+(define (base e) (cadr e))
+
+(define (exponent e) (caddr e))
+
 (display (deriv '(+ x 3) 'x))
+(display (deriv '(* x y) 'x))
+(display (deriv '(* (* x y) (+ x 3)) 'x))
+(display (deriv '(* y (^ x 6)) 'x))
