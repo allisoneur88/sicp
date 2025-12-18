37dfb1d1626842955fac7eebf0a3e125e9730376
allisoneur88 <adganshin@gmail.com>
2025-12-18 23:17:54 +0100

    set representation with binary trees

--- ch2/8_representing_sets.rkt
+++ ch2/8_representing_sets.rkt
@@ -99,3 +99,78 @@
          (cons (car set1) (ord-union-set (cdr set1) set2)))))
 
 (display (ord-union-set ordset ordset2))
+
+; Binary tree
+(define (entry tree)
+  (car tree))
+(define (left-branch tree)
+  (cadr tree))
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-tree? e tree)
+  (cond
+    ((null? tree) false)
+    ((= e (entry tree)) true)
+    ((< e (entry tree))
+     (element-of-tree? e (left-branch tree)))
+    ((> e (entry tree))
+     (element-of-tree? e (right-branch tree)))))
+   
+(define (adjoin-tree e tree)
+  (cond
+    ((null? tree) (make-tree e '() '()))
+    ((= e (entry tree)) tree)
+    ((< e (entry tree))
+     (make-tree
+       (entry tree)
+       (adjoin-tree e (left-branch tree))
+       (right-branch tree)))
+    ((> e (entry tree))
+     (make-tree
+       (entry tree)
+       (left-branch tree)
+       (adjoin-tree e (right-branch tree))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree) '()
+      (append
+       (tree->list-1 (left-branch tree))
+       (cons (entry tree) (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+     result-list
+     (copy-to-list (left-branch tree)
+       (cons (entry tree) (copy-to-list (right-branch tree) result-list)))))
+  (copy-to-list tree '()))
+     
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+       (let ((left-result (partial-tree elts left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-elts (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+         (let ((this-entry (car non-left-elts))
+               (right-result
+                (partial-tree
+                 (cdr non-left-elts)
+                 right-size)))
+          (let ((right-tree (car right-result))
+                (remaining-elts
+                 (cdr right-result)))
+           (cons (make-tree this-entry
+                            left-tree
+                            right-tree)
+                 remaining-elts))))))))
+
+
+(display (tree->list-2(list->tree (list 1 2 3))))
