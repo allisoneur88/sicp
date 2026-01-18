d0f25cd188bf40f0e0d79b1e3c88dd248aea2c74
allisoneur88 <adganshin@gmail.com>
2026-01-18 21:57:36 +0100

    mutex and semaphor

--- ch3/8_concurrency.rkt
+++ ch3/8_concurrency.rkt
@@ -51,4 +51,113 @@
             (else (error "Unknown request" m))))
     dispatch))
 
+; we implement serializers in terms
+; of a more primitive synchronization
+; mechanism called a mutex.
 
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+       (mutex 'acquire)
+       (let ((val (apply p args)))
+        (mutex 'release)
+        val))
+      serialized-p)))
+
+(define (make-test)
+  (lambda (p)
+    (define (tested-p . args)
+      (print! "Mutex acquired")
+      (let ((val (apply p args)))
+       (print! "Mutex released")
+       (newline)
+       val))
+    tested-p))
+
+(define (square x) (* x x))
+(define test (make-test))
+(define tested-square (test square))
+(tested-square 5)
+
+(define (print! string)
+  (newline)
+  (display string))
+
+; Mutex
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+                 (the-mutex 'acquire))) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell) (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      true
+      (begin
+       (set-car! cell true)
+       false)))
+
+; there is a crucial subtlety here
+; the test-and-set! operation
+; must be performed atomically!
+
+; the actual implementation of
+; test-and-set! depends on the
+; details of how our system runs
+; concurrent processes
+
+; in mit-scheme for a single processor,
+; which uses a time-slicing model,
+; test-and-set! can be implemented as follows:
+(define (single-processor-time-slicing-test-and-set! cell)
+  (without-interrupts
+    (lambda ()
+      (if (car cell)
+       true
+       (begin! (set-car! cell true)
+               false)))))
+
+; alternatively, multiprocessing computers
+; provide instructions that support atomic
+; operations directly in hardware
+
+; naive semaphore
+(define (make-semaphore-n n)
+  (let ((counter n))
+    (define (test-and-dec!)
+      (if (= counter 0)
+       true
+       (begin (set! counter (- counter 1))
+              false)))
+    (define (inc!)
+      (set! counter (+ counter 1)))
+    (define (the-semaphore m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-dec!)
+              (the-semaphore 'acquire)))
+            ((eq? m 'release) (inc!))))
+    the-semaphore))
+
+; we need an atomic primitive
+; we use a serializer built 
+; top of atomic primitives
+(define (make-semaphore-n n)
+  (let ((counter n)
+        (s (make-serializer)))
+    (define (acquire)
+      (s (lambda ()
+          (if (> counter 0)
+           (set! counter (- counter 1))
+           (acquire)))))
+    (define (release)
+      (s (lambda ()
+          (set! counter (+ counter 1)))))
+    (lambda (m)
+      (cond ((eq? m 'acquire) (acquire))
+            ((eq? m 'release) (release))))))
