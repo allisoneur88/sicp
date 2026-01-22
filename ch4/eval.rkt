f2c2c17161462cf2376a60ccb829cc6e9b14d000
allisoneur88 <adganshin@gmail.com>
2026-01-22 12:26:53 +0100

    define special form and an update to env

--- ch4/eval.rkt
+++ ch4/eval.rkt
@@ -1,6 +1,7 @@
 #lang sicp
 
-(define *env (make-table))
+; *genv is a list of tables
+(define *genv (list (make-table)))
 
 (define (*eval exp env)
   (cond
@@ -8,6 +9,7 @@
     ((symbol? exp) (lookup exp env))
     ((define? exp) (eval-define exp env))
     ((if? exp) (eval-if exp env))
+    ((lambda? exp) (eval-lambda exp env))
     ((application? exp) (*apply (*eval (car exp) env)
                                (map (lambda (e) (*eval e env)) (cdr exp))))
     (else
@@ -20,18 +22,45 @@
       (scheme-apply (get-scheme-procedure operator) operands)
       (error "Operator not a procedure: " operator)))
 
+(define (*apply operator operands)
+  (cond ((primitive? operator)
+         (scheme-apply (get-scheme-procedure operator) operands))
+        ((compound? operator)
+         (*eval (operator-body operator)
+                (extend-env-with-new-frame
+                 (operator-parameters operator) operands (operator-env operator))))))
+
 (define prim-tag 'primitive)
 (define (make-primitive scheme-proc)
   (list prim-tag scheme-proc))
 (define (primitive? exp) (tag? exp prim-tag))
 (define (get-scheme-procedure prim) (cadr prim))
 
+(define compound-tag 'compound)
+(define (make-compound parameters body env)
+  (list compound-tag parameters body env))
+(define (compound? exp) (tag? exp compound-tag))
+(define (operator-parameters compound-exp) (cadr compound-exp))
+(define (operator-body compound-exp) (caddr compound-exp))
+(define (operator-env compound-exp) (cadddr compound-exp))
+
+(define (extend-env-with-new-frame vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (error "Arguments/Parameters mismatch")))
+(define (make-frame vars vals)
+  (let ((table (make-table)))
+    (for-each (lambda (var val) (put! var val table))
+              vars
+              vals)
+    table))
 
 (define (install-primitives)
   (begin
-    (put-env! '*+ (make-primitive +))
-    (put-env! '*> (make-primitive >))
-    (put-env! '*true #t))
+    (put-genv! '*+ (make-primitive +))
+    (put-genv! '** (make-primitive *))
+    (put-genv! '*> (make-primitive >))
+    (put-genv! '*true #t))
   'ok)
 
 (install-primitives)
@@ -47,6 +76,8 @@
   (tag? exp '*define))
 (define (if? exp)
   (tag? exp '*if))
+(define (lambda? exp)
+  (tag? exp '*lambda))
 
 ; define
 (define (defenee exp)
@@ -54,7 +85,8 @@
 (define (define-value exp)
   (caddr exp))
 (define (eval-define exp env)
-  (put! (defenee exp) (*eval (define-value exp) env) env))
+  (let ((current-frame (car env)))
+    (put! (defenee exp) (*eval (define-value exp) env) current-frame)))
 
 ; if
 (define (predicate if-exp)
@@ -75,25 +107,71 @@
             (else
              (error "Predicate does not eval to #t or #f: EVAL-IF" test))))))
 
+; lambda
+; (*lambda (x) (*+ x x))
+(define (lambda-arguments exp)
+  (cadr exp))
+(define (lambda-body exp)
+  (caddr exp))
+(define (eval-lambda exp env)
+  (make-compound (lambda-arguments exp) (lambda-body exp) env))
+
 (define (lookup thing env)
-  (get thing env))
+  (if (null? env)
+      (error "Unbound variable: " thing)
+      (let ((found (get thing (car env))))
+       (if found
+        found
+        (lookup thing (cdr env))))))
 
 ; tests
-(*eval '(*+ 5 (*+ 2 3)) *env)
+(*eval '(*define fact-iter
+         (*lambda (product counter max-count)
+          (*if (*> counter max-count)
+           product
+           (fact-iter (** product counter) (*+ counter 1) max-count)))) *genv)
+
+(*eval '(*define factorial
+         (*lambda (n)
+          (fact-iter 1 1 n))) *genv)
+
+(*eval '(factorial 5) *genv)
+
+(*eval '(*define factorial
+         (*define fact-iter
+          (*lambda (product counter max-count)
+           (*if (*> counter max-count)
+            product
+            (fact-iter (** product counter) (*+ counter 1) max-count))))
+         (*lambda (n)
+          (fact-iter 1 0 n))) *genv)
+
+(*eval '(factorial 5) *genv)
+
+(*eval '(*define square
+         (*lambda (x) (** x x))) *genv)
+(*eval '(square 5) *genv)
+(*eval '(*+ 5 (*+ 2 3)) *genv)
 
 (*eval
-  '(*define x 5) *env)
+  '(*define x 5) *genv)
 
 (*eval
-  '(*+ x (*+ 2 3)) *env)
+  '(*+ x (*+ 2 3)) *genv)
 
 (*eval
   '(*if (*> x 6)
     x
-    (*+ x 66)) *env)
+    (*+ x 66)) *genv)
 
 (*eval
-  '(*if *true 5 6) *env)
+  '(*if *true 5 6) *genv)
+
+(*eval
+  '(*+ 5 6 7 x) *genv)
+
+(*eval
+  '((*lambda (x) (*+ x x)) 5) *genv)
 
 ; support table
 (define (get key table)
@@ -111,8 +189,8 @@
                      (cdr table)))))
   'ok)
 
-(define (put-env! key value)
-  (put! key value *env))
+(define (put-genv! key value)
+  (put! key value (car *genv)))
 
 (define (make-table)
   (list '*table*))
